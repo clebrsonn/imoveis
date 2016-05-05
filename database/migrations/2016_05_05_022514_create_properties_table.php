cf40c0e864da0516e813443170e5e726483022a7
Cleberson <clebrsonn@hotmail.com>
2016-05-05 11:33:29 -0300

    Update 2016_05_05_022514_create_properties_table.php

--- database/migrations/2016_05_05_022514_create_properties_table.php
+++ database/migrations/2016_05_05_022514_create_properties_table.php
@@ -17,14 +17,14 @@ class CreatePropertiesTable extends Migration
 
             $table->integer('Number_Bedrooms');
             $table->integer('Number_Suites')->nullable();
-            $table->decimal('Area_Util', 20);
-            $table->integer('Spaces_Garage')->nullable();
-            $table->decimal('Value', 20)->nullable();
-            $table->integer('Number_Banheiros')->nullable();
-            $table->decimal('IPTU_Year', 20)->nullable();
-            $table->decimal('Condominio_Mes', 20)->nullable();
-            $table->string('Acess', 45)->nullable();
-            $table->string('Desc_Property', 2000);
+            $table->decimal('area_util', 20);
+            $table->integer('spaces_garage')->nullable();
+            $table->decimal('value', 20)->nullable();
+            $table->integer('number_wc')->nullable();
+            $table->decimal('iptu_Year', 20)->nullable();
+            $table->decimal('cond_month', 20)->nullable();
+            $table->string('access', 45)->nullable();
+            $table->string('property_description', 2000);
 
             $table->timestamps();
 
