c5086d495abeb110a1a04676854384f358b0b3f3
Cleberson <clebrsonn@hotmail.com>
2016-05-05 11:19:05 -0300

    Update Prop_Detail.php

--- app/Models/Prop_Detail.php
+++ app/Models/Prop_Detail.php
@@ -6,6 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Prop_Detail extends Model
 {
+    protected $table = 'prop_details';
+
     public $timestamps = false;
+    
+    protected $fillable = ['prop_detail'];
+
 
 }
