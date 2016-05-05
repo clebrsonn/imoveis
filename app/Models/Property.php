c2be7cd11eb364884c9fb5fa1a87a36adc58bee1
Cleberson <clebrsonn@hotmail.com>
2016-05-05 11:33:51 -0300

    Update Property.php

--- app/Models/Property.php
+++ app/Models/Property.php
@@ -6,5 +6,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class Property extends Model
 {
-    //
+
+    protected $table = 'properties';
+    
+    public $timestamps = false;
+
+    protected $fillable = ['number_bedrooms', 'number_suites', 'area_util', 
+                            'spaces_garage', 'value', 'number_wc', 'iptu_year'
+                            'cond_month', 'access', 'property_description'
+    ];
+
 }
