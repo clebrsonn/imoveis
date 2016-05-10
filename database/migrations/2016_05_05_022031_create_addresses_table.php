8d2335737e593472496ca3fd852c3801034d6b61
Cleberson <clebrsonn@hotmail.com>
2016-05-10 10:20:35 -0300

    Update 2016_05_05_022031_create_addresses_table.php

--- database/migrations/2016_05_05_022031_create_addresses_table.php
+++ database/migrations/2016_05_05_022031_create_addresses_table.php
@@ -19,7 +19,7 @@ class CreateAddressesTable extends Migration
             $table->string('District');
             
             //foreign key
-            $table->string('city_id');
+            $table->integer('city_id')->unsigned();
         });
     }
 
